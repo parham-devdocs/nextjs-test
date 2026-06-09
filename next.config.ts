11dc70338272ea1792ba6df9f63be69f426e3c34
parham <p.77.pazargadi@gmail.com>
2026-06-09 23:27:27 +0330

    next config changed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  logging: {
+    level: 'verbose',
+    fullUrl: true
+  }
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
